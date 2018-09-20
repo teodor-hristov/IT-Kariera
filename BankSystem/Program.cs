699652518fb17b7118197967baeb8b165e2c93c0
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-20 21:32:25 +0300

    Testing version control settings

---
+++ BankSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
