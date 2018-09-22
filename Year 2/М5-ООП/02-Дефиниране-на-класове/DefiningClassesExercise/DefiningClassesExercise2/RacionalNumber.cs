818c18f053e12aaa9d9b1184f2985fa40a6295f5
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-22 21:21:28 +0300

    Exercise 2 finished

---
+++ Year 2/М5-ООП/02-Дефиниране-на-класове/DefiningClassesExercise/DefiningClassesExercise2/RacionalNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClassesExercise2
+{
+    class RacionalNumber
+    {
+        private List<RacionalNumber> racionalNumbersList = new List<RacionalNumber>();
+        private List<int> getFinalNumberList;
+
+
+        public RacionalNumber() { }
+        public RacionalNumber(int numerator, int denumerator)
+        {
+            getFinalNumberList = new List<int>();
+            this.Numerator = numerator;
+            this.Denumerator = denumerator;
+            getFinalNumberList.Add(Numerator / BiggestDivider(Numerator, Denumerator));
+            getFinalNumberList.Add(Denumerator / BiggestDivider(Numerator, Denumerator));
+        }
+
+
+        public int Numerator { get; set; }
+        public int Denumerator { get; set; }
+
+
+        private int BiggestDivider(int numerator, int denumerator)
+        {
+            if (denumerator == 0)
+            {
+                return numerator;
+            }
+            else
+            {
+                return BiggestDivider(denumerator, numerator % denumerator);
+            }
+        }
+
+
+        public void Print()
+        {
+            for (int i = 1; i < getFinalNumberList.Count; i++)
+            {
+                Console.WriteLine($"{getFinalNumberList[i-1]}/{getFinalNumberList[i]}");
+            }
+        }
+
+
+    }
+}
---
+++ Year 2/М5-ООП/02-Дефиниране-на-класове/DefiningClassesExercise/DefiningClassesExercise2/SetUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClassesExercise2
+{
+    class SetUp
+    {
+        public static void AddRacionalNumbers(int[] numbers)
+        {
+            int numerator;
+            int denumerator;
+            RacionalNumber racionalNumber;
+
+            for (int i = 1; i < numbers.Length; i+=2)
+            {
+                numerator = numbers[i-1];
+                denumerator = numbers[i];
+                racionalNumber = new RacionalNumber(numerator,denumerator);
+                racionalNumber.Print();
+               
+            }
+            
+
+        }
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            AddRacionalNumbers(numbers);
+            
+            
+        }
+    }
+}
