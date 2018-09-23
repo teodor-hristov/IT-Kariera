ce7a25fafccc6122200c67ba138f3fec26111834
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-23 20:04:27 +0300

    Exercise 1 completed

---
+++ Year 2/М5-ООП/03-Шаблонни-класове/Generics/GenericMethodsAndInterfaces/SetUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericMethodsAndInterfaces
+{
+    class SetUp
+    {
+        static void Main()
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+            foreach (var item in strings)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
