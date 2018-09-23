e1f18b3cb33f4e0a267ca98123b7ef82b017b118
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-23 18:51:32 +0300

    Completed Generics Exercise 1

---
+++ Year 2/М5-ООП/03-Шаблонни-класове/Generics/Exercise-2/SetUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class SetUp
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
---
+++ Year 2/М5-ООП/03-Шаблонни-класове/Generics/Generics/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    class Box<T> : IBox<T>
+    {
+        int DEFAULT_LIST_SIZE = 0;
+        private List<T> itemHolder;
+
+        public List<T> ItemHolder
+        {
+            get { return itemHolder; }
+            set { itemHolder = value; }
+        }
+
+        public Box()
+        {
+            itemHolder = new List<T>();
+            Count = DEFAULT_LIST_SIZE;
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(T element)
+        {
+            itemHolder.Add(element);
+        }
+
+
+        public T Remove()
+        {
+            T item = ItemHolder.Last();
+            ItemHolder.Remove(item);
+            return item;
+        }
+    }
+}
