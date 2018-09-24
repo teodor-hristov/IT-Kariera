2f245d9dba61e52498436ac764bad8936f2a704a
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-24 15:08:20 +0300

    Exercise 1 completed

---
+++ Year 2/М5-ООП/04-Наследяване-абстракция-интерфейси/Exercises/Exercises_04.2/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_04._2
+{
+    class RandomList : ArrayList
+    {
+        public int RandomNum { get => new Random().Next(0, base.Count-1); }
+
+        public string RandomString()
+        {
+            string item;
+            int rnd = RandomNum;
+            item = base[rnd].ToString();
+            base.RemoveAt(rnd);
+            return item;
+        }
+    }
+}
---
+++ Year 2/М5-ООП/04-Наследяване-абстракция-интерфейси/Exercises/Exercises_04.2/SetUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercises_04._2
+{
+    class SetUp
+    {
+        static void Main()
+        {
+            RandomList rndList = new RandomList();
+
+            rndList.Add("test1");
+            rndList.Add("test2");
+            rndList.Add("test3");
+            rndList.Add("test4");
+            rndList.Add("test5");
+
+            foreach (string item in rndList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("//"+rndList.RandomString());
+
+            foreach (string item in rndList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+}
