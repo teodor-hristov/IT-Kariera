35081bbd6cb729cafc2168fbe06caa44f8f97349
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-24 14:15:13 +0300

    Exercise 3 completed

--- Year 2/М5-ООП/04-Наследяване-абстракция-интерфейси/Exercises/Exercises/SetUp.cs
+++ Year 2/М5-ООП/04-Наследяване-абстракция-интерфейси/Exercises/Exercises/SetUp.cs
@@ -10,6 +10,10 @@ namespace Exercises
             puppy.Eat();
             puppy.Bark();
             puppy.Weep();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
         }
     }
 }
