6733c7240a57395c9b1571e2c19359bf43cdbaf2
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-24 14:09:59 +0300

    Exercise 1 completed

---
+++ Year 2/М5-ООП/04-Наследяване-абстракция-интерфейси/Exercises/Exercises/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises
+{
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+}
---
+++ Year 2/М5-ООП/04-Наследяване-абстракция-интерфейси/Exercises/Exercises/SetUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercises
+{
+    class SetUp
+    {
+        static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+}
