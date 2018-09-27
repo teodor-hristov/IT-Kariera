d025122bbe2bb91b3218d911dda457a9f7e0eb42
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-28 01:42:28 +0300

    Task 1 finished

---
+++ Year 2/М5-ООП/05-Полиморфизъм-в-ООП/Exercises_05.1/Exercises_05.1/MathOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises_05._1
+{
+    class MathOperations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+        public decimal Add(decimal a, decimal b, decimal c)
+        {
+            return a + b + c;
+        }
+    }
+}
