832634cc1bd6d9645a87ef314b1cfedf7b60e1ce
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-28 22:58:35 +0300

    Exercise 5 completed //todo: remove dummy code

---
+++ Year 2/М5-ООП/06-Работа-с-обекти/Exercises/Exercise_06.5/App.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_06._5
+{
+    class App
+    {
+        static void Main()
+        {
+            Stack<string> stack = new Stack<string>("pesho","gosho","qsen");
+            foreach (string item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            stack.Push("pesho1", "gosho1", "qsen1");
+            stack.Pop();
+            foreach (string item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Year 2/М5-ООП/06-Работа-с-обекти/Exercises/Exercise_06.5/Stack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercise_06._5
+{
+    class Stack<T>
+    {
+        private List<T> container;
+
+        private class StackIterator<T> : IEnumerator<T>
+        {
+            private readonly List<T> container;
+            private int counter;
+            public StackIterator(List<T> container)
+            {
+                this.Reset();
+                container.Reverse();
+                this.container = container;
+                
+            }
+            public T Current => this.container[counter];
+            object IEnumerator.Current => this.Current;
+            public void Dispose(){}
+            public bool MoveNext() => this.counter++ < this.container.Count-1;
+            public void Reset() => this.counter = -1;
+        }
+
+        public Stack(params T[] items)
+        {
+            container = new List<T>(items);
+        }
+
+        public T Pop()
+        {
+            T temp = container.Last();
+            container.Remove(temp);
+            return temp;
+        }
+
+        public T Peek()
+        {
+            T temp = container.Last();
+            return temp;
+        }
+
+        public void Push(params T[] item)
+        {
+            for (int i = 0; i < item.Length; i++)
+            {
+                container.Add(item[i]);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new StackIterator<T>(this.container);
+        }
+
+    }
+}
