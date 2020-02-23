3c8915f9dbcbfc735788c8ab48d02bb0d273c664
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-02-23 10:23:38 +0200

    Add team model

--- Year 3/Software dev/VacationManager/VacationManager/Models/Team.cs
+++ Year 3/Software dev/VacationManager/VacationManager/Models/Team.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace VacationManager.Models
 {
     public class Team
     {
+        [Key]
+        public String Id { get; set; }
+        public String Name { get; set; }
+        public Project Project { get; set; }
+        public List<User> Developers { get; set; }
+        public User TeamLeader { get; set; }
     }
 }
