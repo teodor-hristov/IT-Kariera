438c89ad23ea1d8bdc6ca601a4bc111bbb0554f7
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-02-23 10:23:58 +0200

    Add project model

---
+++ Year 3/Software dev/VacationManager/VacationManager/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VacationManager.Models
+{
+    public class Project
+    {
+        [Key]
+        public String Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public List<Team> Teams { get; set; }
+    }
+}
