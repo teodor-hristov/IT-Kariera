6fabc2d96fed344156f1a841e58619a5437b708a
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-02-23 10:23:48 +0200

    Add user model

---
+++ Year 3/Software dev/VacationManager/VacationManager/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VacationManager.Models
+{
+
+    public class User
+    {
+        [Key]
+        public String Id { get; set; }
+        public String Password { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String Role { get; set; }
+        public Team Team { get; set; }
+    }
+}
