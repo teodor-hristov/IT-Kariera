263cb004c460ae88a19b7a76bfd55861b39d45b8
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2020-02-28 16:30:06 +0200

    MyLibrary

---
+++ Year 3/Web/MyLibrary/Data/MyLibraryDb.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class MyLibraryDb : DbContext
+    {
+        public virtual DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=MyLibraryDb;");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
